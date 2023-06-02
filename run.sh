cb77768407ae27b56c909fc015a9d79b995caf69
Carsten Grzemba <grzemba@contac-dt.de>
2023-06-02 12:30:05 +0200

    run.sh change repo path

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ zfs destroy -r rpool/kayak_image/r151044
 zfs destroy -r rpool/kayak_image/miniroot
 rm -r /kayak_image/*
 cd ~admin/github/kayak/
-gmake PKGURL=/home/admin/github/illumos-omnios/packages/i386/nightly/repo.redist PREBUILT_ILLUMOS=/home/admin/github/illumos-omnios BUILDSEND=rpool/kayak_image  install-iso
+gmake PKGURL=/lhome/admin/github/illumos-omnios/packages/i386/nightly/repo.redist PREBUILT_ILLUMOS=/lhome/admin/github/illumos-omnios BUILDSEND=rpool/kayak_image  install-iso
